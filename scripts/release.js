78bd0aa7b450e803e62092d10ea6a165d0b9f2ef
Samuele Scatena <samuele.scatena@gmail.com>
2024-04-09 10:00:11 +0200

    fixed camelcase file name in DfuNotificationActivity

--- scripts/release.js
+++ scripts/release.js
@@ -5,12 +5,14 @@ const { name, version } = require('../package.json');
 const { execSync } = require('child_process');
 const root = path.resolve(__dirname, '..');
 const dest = path.join(root, `example/node_modules/${name}`);
+const bin = path.join(root, `bin/`);
 
 console.log('PRE FLIGHT SCRIPT ', name, version);
 
 function pre() {
-  console.log('pre ', name, version);
+  console.log('pre ', root, dest, name, version);
   let oldPath = path.join(root, `${name}-${version}.tgz`);
+  console.log('aft ', oldPath);
   if (fs.existsSync(oldPath)) fs.rmSync(oldPath);
   oldPath = path.join(root, `${name}`);
   if (fs.existsSync(oldPath)) fs.rmSync(oldPath, { recursive: true });
@@ -22,10 +24,42 @@ function move() {
   fs.renameSync(path.join(root, 'package'), `${name}`);
 }
 
+function copyFiles(sourceDir, targetDir) {
+  // Create target directory if it doesn't exist
+  if (!fs.existsSync(targetDir)) {
+    fs.mkdirSync(targetDir);
+  }
+
+  // Read all files and directories in the source directory
+  fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+
+    // Iterate through each file/directory
+    files.forEach((file) => {
+      const sourcePath = path.join(sourceDir, file.name);
+      const targetPath = path.join(targetDir, file.name);
+
+      // Check if it's a file or a directory
+      if (file.isDirectory()) {
+        // If it's a directory, recursively copy it
+        copyFiles(sourcePath, targetPath);
+      } else {
+        // If it's a file, copy it
+        fs.copyFile(sourcePath, targetPath, (err) => {
+          if (err) throw err;
+          console.log(`${file.name} copied successfully.`);
+        });
+      }
+    });
+  });
+}
+
 function copy() {
   console.log('copy ', name, version);
   if (fs.existsSync(dest)) fs.rmSync(dest, { recursive: true });
-  fs.renameSync(path.join(root, `${name}`), dest);
+  fs.mkdirSync(dest);
+  copyFiles(path.join(root, `${name}`), dest);
+  // fs.renameSync(path.join(root, `${name}`), dest);
 }
 
 pre();
